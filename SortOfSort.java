ba890df603e89d9babda14cf4240a9d610a29e79
Admunoz8 <60449029+Admunoz8@users.noreply.github.com>
2020-03-05 23:01:31 -0700

    Add files via upload
    need to comment on tester and start on document.

--- SortOfSort.java
+++ SortOfSort.java
@@ -1,26 +1,60 @@
-public class SortOfSort{
-    public static void sortOfSort(int x[]){
-        for(int i=0;i<x.length;i++){
-            for(int k=0;k<x.length;k++)
-                if(x[i]>x[k])
-                {
-                    int temp = x[i];
-                    x[i] = x[k];
-                    x[k] = temp;
-                }
-        }
-        int y[] = new int[x.length];
-        System.arraycopy(x,0,y,0,x.length);
-        int z[] = {3,4,7,8,6,5,2,1};
-        for(int i=0;i<x.length;i++){
-            x[i] = y[z [i] - 1];
-        }
-        for(int i = 0; i < x.length; i++){
-            System.out.print(x[i]+" ");
-        }
 
+public class SortOfSort {
+    public static int[] sortOfSort(int[] arr) {
+        int index = 0;
+        int switcher = 0;
+        int start = 0;
+        int end = arr.length-1;
+        int temp;
+
+     //iterating
+      for (int i=0;i < arr.length-1;i++){
+          index = i;
+          int max = Integer.MIN_VALUE;
+
+            //Find largest
+            for (int k = start; k <= end; k++)
+                if (arr[k] >= max) {
+                    //biggest number
+                    max = arr[k];
+                    //biggest number
+                    index = k;
+                }
+            //If statement to  add elements to end
+            if (switcher < 2) {
+
+                // Swapping the largest to the end
+                temp = arr[index];
+                arr[index] = arr[end];
+                arr[end] = temp;
+                switcher++;
+                end = end - 1;
+            }
+            else {
+                // Swapping the largest with the first element for the next 2 times
+                 temp = arr[index];
+                arr[index] = arr[start];
+                arr[start] = temp;
+                start = start + 1;
+                switcher++;
+            }
+
+            if (switcher == 4) {
+                switcher = 0;
+            }
+
+        }
+        return arr;
     }
 
+    // Prints the array
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; ++i)
+            System.out.print(arr[i] + " ");
+    }
+    public static void main(String[] args) {
+        int array[] = {3,4,5,6,2,1};
+        sortOfSort(array);
+        print(array);
+    }
 }
-
-
--- SortOfSortTest.java
+++ SortOfSortTest.java
@@ -1,39 +1,72 @@
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-class SortOfSortTest {
+public class SortOfSortTest {
     @Test
-    public void TestSort1(){
-        int[] arr = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
-        for(int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println("");
-        int[] arrExpected = new int[] {6, 5, 2, 1, 3, 4, 7, 8};
-        for(int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println("");
+    public void tester1() {
+        int[] inputArray = {0};
+        int[] expectedOutput = {0};
+
+        SortOfSort testOne = new SortOfSort();
+        testOne.sortOfSort(inputArray);
+
+        for (int i = 0; i < inputArray.length; i++) {
+            assertEquals(expectedOutput[i], inputArray[i]);
+        }
+
     }
     @Test
-    public void TestSort2(){
-        int[] arr = new int[] {9,9,9,9,9,9,9,9,9,9,9,9,9};
-        for(int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println("");
-        int[] arrExpected = new int[] {9,9,9,9,9,9,9,9,9,9,9,9,9};
-        for(int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println("");
+    public void tester2() {
+        int[] inputArray = {3,4,5,6,2,1};
+        int[] expectedOutput = {4,3,1,2,5,6 };
+
+        SortOfSort testOne = new SortOfSort();
+        testOne.sortOfSort(inputArray);
+
+        for (int i = 0; i < inputArray.length; i++) {
+            assertEquals(expectedOutput[i], inputArray[i]);
+        }
+
+
     }
     @Test
-    public void TestSort3(){
-        int[] arr = new int[] {0};
-        for(int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println("");
-        int[] arrExpected = new int[] {0};
-        for(int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println("");
+    public void tester3() {
+        int[] inputArray = {-2,-3,-3,-4,-5,-6,13,45,-4};
+        int[] expectedOutput = {-2,-3,-4,-5,-6,-4,-3,13,45 };
+
+        SortOfSort testOne = new SortOfSort();
+        testOne.sortOfSort(inputArray);
+
+        for (int i = 0; i < inputArray.length; i++) {
+            assertEquals(expectedOutput[i], inputArray[i]);
+        }
+
+    }
+    @Test
+    public void tester4() {
+        int[] inputArray = {99,100,200,400,500,900,1000};
+        int[] expectedOutput = {500,400,99,100,200,900,1000 };
+
+        SortOfSort testOne = new SortOfSort();
+        testOne.sortOfSort(inputArray);
+
+        for (int i = 0; i < inputArray.length; i++) {
+            assertEquals(expectedOutput[i], inputArray[i]);
+        }
+
+    }
+    @Test
+    public void tester5() {
+        int[] inputArray = {1,1,1,1,1,1,1,1,1,1};
+        int[] expectedOutput = {1,1,1,1,1,1,1,1,1,1};
+
+        SortOfSort testOne = new SortOfSort();
+        testOne.sortOfSort(inputArray);
+
+        for (int i = 0; i < inputArray.length; i++) {
+            assertEquals(expectedOutput[i], inputArray[i]);
+        }
+
     }
 
 }
